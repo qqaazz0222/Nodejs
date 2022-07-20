5afa002c1481a98b3c009809515892927a5d467e
qqaazz0222 <qqaazz0222@naver.com>
2022-07-21 03:03:38 +0900

    Node.js 공부
    2일차

--- test/routes/index.js
+++ test/routes/index.js
@@ -1,49 +1,44 @@
+const e = require('express');
 var express = require('express');
 var router = express.Router();
 var connection = require('../db/db');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index');
 });
 
-router.get('/signin', (req, res, next) => {
+router.get('/signin', (req, res) => {
   res.render('signin');
 });
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin', (req, res) => {
   var id = req.body.id;
   var pw = req.body.pw;
   res.send(`id : ${id}, pw : ${pw}`);
-  // res.json(req.body);
 });
 
-router.get('/signup', (req, res, next) => {
+router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', (req, res) => {
   var id = req.body.id;
   var pw = req.body.pw;
   var name = req.body.name;
-  var temp_id = '';
-  connection.query('SELECT id FROM users WHERE id = ?;', [id], (err, res, fld) => {
-    if(err) throw err;
-    temp_id = res[0].id;
-    console.log(`res : ${res[0].id}`);
+  connection.query('INSERT INTO users VALUES (?, ?, ?);', [id, pw, name], (err1, res1, fld1) => {
+    if(err1) {
+      if(err1.errno==1062) {
+        console.log('이미 등록된 아이디입니다.');
+        res.render('signup');
+        throw err1;
+      }
+      throw err1;
+    }
+    else{
+      res.render('signup_complete', { name: name });
+    }
   })
-  console.log(`temp_id : ${temp_id}`);
-  console.log(`id : ${id}`);
-  if(id == temp_id) {
-    console.log('중복된 아이디 입력');
-  }
-  else {
-    connection.query('INSERT INTO users VALUES ( ?, ?, ?);', [id, pw, name], (err, res, fld) => {
-      if(err) throw err;
-      console.log(res);
-    })
-    res.render('signup_complete', { name: name });
-  }
 });
 
 
