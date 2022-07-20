37b6b1210c82cf7a6f0e1ae48d5d58617d5742ad
qqaazz0222 <qqaazz0222@naver.com>
2022-07-21 00:12:35 +0900

    Node.js 공부
    1일차

---
+++ login/middleware/db.js
@@ -0,0 +1,10 @@
+// var mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '11111111',
+//     database: 'board'
+//   })
+
+// module.exports = connection;
