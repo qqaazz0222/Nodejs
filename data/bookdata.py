4ab2cbd30b0ffa2c9abde5f18469af254cc355ea
qqaazz0222 <qqaazz0222@naver.com>
2022-10-13 14:11:43 +0900

    Create bookdata.py

---
+++ data/bookdata.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.naver.com")  
+
+bsObject = BeautifulSoup(html, "html.parser") 
+
+
+print(bsObject) # 웹 문서 전체가 출력됩니다. 
+
