873bd40edc1ccabd175d427534da4419703f7a64
qqaazz0222 <qqaazz0222@naver.com>
2022-10-13 15:31:31 +0900

    Update bookdata.py

--- data/bookdata.py
+++ data/bookdata.py
@@ -1,10 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.naver.com")  
-
-bsObject = BeautifulSoup(html, "html.parser") 
-
-
-print(bsObject) # 웹 문서 전체가 출력됩니다. 
-
+print("start")
+html = urlopen("http://www.yes24.com/24/Category/BestSeller")  
+bsObject = BeautifulSoup(html, "html.parser")
+bookname = []
+for i in range(1, 41):
+	print(str(i) + "start")
+	temp = bsObject.select_one('#bestList > ol > li.num' + str(i) + ' > p:nth-child(3) > a')
+    print(temp)
+	bookname.append(temp.get_text())
+	print(str(i) + "fin")
+print(bookname) # 웹 문서 전체가 출력됩니다. 
